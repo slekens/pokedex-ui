db05456f544ea0b5142b6810f6d6e9a8513fe232
Abraham Abreu <slek_2001@hotmail.com>
2022-03-11 17:10:13 -0600

    First commit

---
+++ Pokedex-UI/Pokedex-UI/Pokedex_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_UIApp.swift
+//  Pokedex-UI
+//
+//  Created by Abraham Abreu on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
